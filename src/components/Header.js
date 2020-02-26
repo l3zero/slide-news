d372c530a29785eb6e9659379a0e60a806a3f9a8
Leonid Yanchis <leonid.yanchis@gmail.com>
2020-02-26 13:34:16 -0500

    Building out page structure using react hooks; Implementing SCSS for this project

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { useState, useContext, useEffect, useCallback, useMemo } from 'react';
+import '../styles/header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Slide News</h1>
+        </header>
+
+    )
+}
+
---
+++ src/components/Intro.js
@@ -0,0 +1,8 @@
+import React, { useState, useContext, useEffect, useCallback, useMemo } from 'react';
+import '../styles/intro.css'
+
+export default function Intro() {
+    return (
+        <div>Customize your news...</div>
+    )
+}
