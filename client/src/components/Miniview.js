e0efe9f23c8e4fb80d50abd6c32e53c3ff2ca24f
Leonid Yanchis <leonid.yanchis@gmail.com>
2020-05-31 19:19:50 -0400

    Finalized mini view + clear options view; Modularized some more css; Tested delete path for DB

---
+++ client/src/components/Miniview.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {httpInits} from '../data/mongoHttpObj'
+import ArticleList from './ArticleList.js'
+import fetch, {Request} from 'node-fetch'
+import {checkStatus} from '../helpers/httpStatusCheck'
+import '../styles/miniview.css'
+
+export default function Miniview(props) {
+   return (
+      <React.Fragment>
+         <div className='menu-container'>
+            <img id='menu-slide' src={require('../img/slide.svg')} alt='' />
+            <div className='menu-content'>
+               <div id='menu-articles' onClick={viewArticleList}>
+                  My Articles
+               </div>
+               <div id='menu-clear' onClick={showWarning}>
+                  Clear Options
+               </div>
+            </div>
+         </div>
+         <ArticleList articles={props.articles} />
+         <div className='options-warning'>
+            <span className='close' onClick={closeWarning}>
+               &times;
+            </span>
+            <span>Delete your options and start fresh?</span>
+            <div>
+               <button id='options-yes' onClick={clearOptions}>
+                  Yes
+               </button>
+               <button id='options-no' onClick={closeWarning}>
+                  No
+               </button>
+            </div>
+         </div>
+      </React.Fragment>
+   )
+
+   function viewArticleList(e) {
+      document.getElementById('my-articles').style.display = 'flex'
+   }
+   function showWarning(e) {
+      e.preventDefault()
+      document.getElementsByClassName('options-warning')[0].style.display = 'flex'
+   }
+   function closeWarning() {
+      document.getElementsByClassName('options-warning')[0].style.display = 'none'
+   }
+   function clearOptions(e) {
+      e.preventDefault()
+      fetch(
+         new Request(
+            `${window.location.origin}/mynews/delete/${JSON.parse(window.localStorage.getItem('myNewsOptions')).id}`,
+            httpInits().DELETE
+         )
+      )
+         .then(checkStatus)
+         .catch((err) => {
+            console.log(err.statusText)
+         })
+
+      window.localStorage.removeItem('myNewsOptions')
+      window.localStorage.removeItem('firstTime')
+      document.location.replace('/')
+   }
+}
