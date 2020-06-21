60925290c11b8648321cfb5c3f98f99545cb5875
Leonid Yanchis <leonid.yanchis@gmail.com>
2020-06-21 00:22:15 -0400

    Updated few tings for heroku db deployment

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose')
 //mongoose.set('autoIndex', false)
 // mongoose.set('bufferCommands', false)
 // mongoose.set('autoCreate', false)
-const userName = process.env['MONGODB_USER']
-const pw = process.env['MONGODB_PW']
-const url = `mongodb://${userName}:${pw}@127.0.0.1:27017/news`
+// const userName = process.env['MONGODB_USER'] for dev only
+// const pw = process.env['MONGODB_PW'] for dev only
+// const url = `mongodb://${userName}:${pw}@127.0.0.1:27017/news` for dev only
+const url = process.env['MONGODB_URI'] || 'mongodb://localhost/slide-news'
 
 mongoose
    .connect(url, {useNewUrlParser: true, useUnifiedTopology: true, keepAliveInitialDelay: 30000})
