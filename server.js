bd41abb1e024b5abc382ebf52518950674ece464
Leonid Yanchis <leonid.yanchis@gmail.com>
2020-06-23 00:55:34 -0400

    Added rate limiting for DB api endpoint; Opting in for PWA - need to customize it in next commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express'),
    bodyParser = require('body-parser'),
    compression = require('compression'),
+   rateLimit = require('express-rate-limit'),
    helmet = require('helmet'),
    cors = require('cors'),
    path = require('path'),
@@ -9,6 +10,7 @@ const express = require('express'),
    newsRouter = require('./routes/newsRouter')
 const app = express()
 app.disable('x-powered-by')
+app.set('trust proxy', 1)
 //DB Stuff
 db.once('open', (_) => {
    console.log('Database connected')
@@ -34,46 +36,22 @@ const corsOptions = {
 }
 app.use(helmet())
 app.use(helmet.referrerPolicy({policy: 'same-origin'}))
-// app.use(
-//    helmet.contentSecurityPolicy({
-//       directives: {
-//          // defaultSrc: ["'self'"],
-//          scriptSrc: ["'unsafe-hashes'", "'unsafe-inline'"],
-//          fontSrc: ['https://fonts.googleapis.com/'],
-//          styleSrc: ["'self'", 'https://fonts.googleapis.com/'],
-//          formAction: ["'self'"],
-//          frameAncestors: ["'self'"],
-//          frameSrc: ["'self'"],
-//       },
-//    })
-// )
 app.use(compression())
 app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-// Handle 404
-// app.use(function (req, res) {
-//    res.status(400)
-//    res.json({title: '404: File Not Found', message: err.message, error: {}})
-// })
-
-// Handle 500
-// app.use(function (err, req, res, next) {
-//    res.status(500)
-//    res.json({title: '500: Internal Server Error', message: err.message, error: {}})
-// })
-app.use('/mynews', newsRouter)
+const apiLimiter = rateLimit({
+   windowMs: 10 * 60 * 1000, // 10 minutes
+   max: 100,
+   message: 'The server has received more than 100 requests in 10 minutes, chill out  for a few :)',
+})
+app.use('/mynews', newsRouter, apiLimiter)
 
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('*', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-//Dev
-// const port = process.env.PORT || '9000'
-// const server = app.listen(port, () => {
-// console.log(`App running → PORT ${port}`)
-// })
-//Prod
+
 const port = process.env.PORT || '9000'
 app.listen(port)
